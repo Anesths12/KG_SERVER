9e1df7c62ee303d4a35f32a70f7252df336f5746
Anesths12 <anesths682@gmail.com>
2025-04-30 21:53:30 +0300

    Delete blog directory

--- blog/js.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    const pageLoader = document.querySelector('.page-loader');
-    setTimeout(function() {
-        pageLoader.classList.add('fade-out');
-    }, 500);
-});
